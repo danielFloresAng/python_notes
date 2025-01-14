8b86adbeb139f12d06e57f569fce7c41fc295c40
Daniel Flores <daniel.flores.ang@gmail.com>
2025-01-14 07:44:19 -0600

    notes

---
+++ basics.py
@@ -0,0 +1,26 @@
+
+
+# THE BACKSLASH (\) -> is called the escape character.
+
+# The backslash doesn't mean anything in itself, but is only a kind of announcement, that the next character after the backslash has a different meaning too.
+
+#In this example, after we put the backslash (\), the second part of the sentence sentence will appears on the next line:
+
+#print("Black keys white keys, \nbettet friends understand.")
+      # Output: 
+          #   Black keys white keys, 
+          #   bettet friends understand.
+#___________________________________________________________________________________________________
+
+# KEYWORD ARGUMENTS -> Are another mechanism for the passing of arguments.
+
+# /// end= ---> The end keyword argument determines the characters the print() function sends to the output once it reaches the end of its positional arguments. 
+
+# print('Hello, mate', end=', ')
+# print('How are you?')
+      #Output: Hello, mate, How are you?
+
+# /// sep= ---> The sep keyword function separates its outputted arguments with a characther given. 
+
+print('eva','00','Rei',sep='_')
+      #Output: eva_00_Rei
